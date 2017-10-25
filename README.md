174207a10684a507a8436d3fde241a3fa1cc7ef0
kalra <CEO@karainfosystems.com>
2017-10-24 21:06:56 -0500

    0.01

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # mv-db
+
+This is basic database that will govern how the backend will look at things. 
+There will be database specific to mysql, cassandra and elastic-search.
+
+Note: This repo is of no purely for demo purposes, and things are not stable if
+verion hasn't reached 1.0.
+
+Current Version 0.01
+
+-- added mysql db schema.
+
---
+++ mysql_schema.sql
@@ -0,0 +1,52 @@
+drop database if exists mvdb;
+create database if not exists mvdb;
+use mvdb;
+create table if not exists mvdb_person (
+	id        bigint auto_increment primary key not null,
+	email     varchar(200) not null,
+	f_name    varchar(100),
+	l_name    varchar(100),
+	createdon date
+);
+	
+
+create table if not exists mvdb_item (
+	id        			bigint auto_increment primary key not null,
+	name      			varchar(100),
+	alias_name          varchar(100),
+	category  			varchar(100),
+	subcategory 		varchar(100),
+	subsubcategory 		varchar(100),
+	subsubsubcategory 	varchar(100),
+	region_country      varchar(100),
+	region_state        varchar(100),
+	region_city         varchar(100),
+	region_pin          varchar(10),
+	manufacturor        varchar(100),
+	owner_name          varchar(100),
+	createdon           date,
+	expiryon            date,
+	has_expired         boolean,
+	itemurl             varchar(400),
+	cost                bigint,
+    currency            enum('us$','inr','cad$','aus$','yuan','yen')
+);
+
+create table if not exists mvdb_review_rating_relationship (
+	id       			bigint auto_increment primary key not null,
+
+	fk_person_id        bigint not null,
+	fk_item_id 			bigint not null,
+
+	foreign key (fk_person_id) references mvdb_person(id) on delete restrict,
+	foreign key (fk_item_id) references mvdb_item(id) on delete cascade,
+
+	my_relationship_with_item    enum('Owned', 'Created', 'Shared', 'Viewed', 'Used'),
+	rating              int, -- [+10 to -10]
+	comments 			varchar(3000),
+	pros				varchar(1000),
+	cons				varchar(1000),
+	createdon			date,
+	relationshipdate	date,
+	hide_details		boolean
+);
